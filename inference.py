1141acee9e2b048ff2b337c6a509c7b9f240d143
tjiagoM <tiago.manuel@gmail.com>
2020-12-30 23:30:24 +0000

    preparing nacc data inference analysis

--- inference.py
+++ inference.py
@@ -1,3 +1,4 @@
+import argparse
 from typing import Tuple
 
 import numpy as np
@@ -40,7 +41,7 @@ def mc_passes(model, loader, n_passes: int, device: str) -> Tuple[np.ndarray, np
     return np.hstack(id_predictions), np.hstack(mean_predictions), np.hstack(std_predictions)
 
 
-def run_on_ukb():
+def run_inference(dataset_location, dataset_id):
     device = torch.device('cuda')
     run_id = 'tjiagom/adni_phenotypes/2cxy59fk'
     api = wandb.Api()
@@ -48,21 +49,38 @@ def run_on_ukb():
 
     model = SimpleMLP(dim_in=155, dropout_rate=best_run.config['dropout']).to(device)
 
-    restored_path = wandb.restore('simple_mlp.pt', run_path=run_id)
+    restored_path = wandb.restore('simple_mlp.pt', replace=True, run_path=run_id)
     model.load_state_dict(torch.load(restored_path.name))
     model.eval()
 
     enable_dropout(model)
 
-    ukb_dataset = BrainFeaturesDataset('data/ukb_scaled_corrected.csv', has_target=False, keep_ids=True)
-    ukb_loader = DataLoader(ukb_dataset, batch_size=200, shuffle=False)
+    dataset = BrainFeaturesDataset(dataset_location, has_target=False, keep_ids=True)
+    loader = DataLoader(dataset, batch_size=200, shuffle=False)
 
-    ids, means, stds = mc_passes(model, ukb_loader, 50, device)
+    ids, means, stds = mc_passes(model, loader, 50, device)
 
-    ret_df = pd.DataFrame(list(zip(ids, means, stds)), columns=['ukb_id', 'mean', 'std']).set_index('ukb_id')
+    ret_df = pd.DataFrame(list(zip(ids, means, stds)), columns=[f'{dataset_id}_id', 'mean', 'std'])
+    ret_df = ret_df.set_index(f'{dataset_id}_id')
 
-    ret_df.to_csv('results/latest_output.csv')
+    ret_df.to_csv(f'results/latest_output_{dataset_id}.csv')
 
+def parse_args():
+    parser = argparse.ArgumentParser(description='ADNI Phenotypes')
+    parser.add_argument('--dataset_location',
+                        type=str,
+                        choices=['data/ukb_scaled_corrected.csv', 'data/nacc_scaled_corrected.csv'],
+                        help='The location of the dataset.')
+
+    parser.add_argument('--dataset_id',
+                        type=str,
+                        choices=['ukb', 'nacc'],
+                        help='Small identification of dataset.')
+
+    return parser.parse_args()
 
 if __name__ == '__main__':
-    run_on_ukb()
+    args = parse_args()
+    print(args)
+
+    run_inference(dataset_location=args.dataset_location, dataset_id=args.dataset_id)
